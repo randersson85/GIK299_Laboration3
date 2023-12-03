220d92741510ae41ab6f6a415bbfc4666faf377f
Robert Andersson <h23roban@du.se>
2023-12-03 19:05:47 +0100

    ändrat hårfärg på person

--- GIK299_L3/Program.cs
+++ GIK299_L3/Program.cs
@@ -7,7 +7,7 @@
             string name = "Alex";
             Gender gender = Gender.ickeBinär;
             string eyeColor = "Blå";
-            Hair hair = new Hair { Haircolor = "Svart", HairLength = "Kort" };
+            Hair hair = new Hair { Haircolor = "Blond", HairLength = "Kort" };
             DateTime birthdate = new DateTime(2009, 2, 25);
 
             Person person = new Person(name, gender, eyeColor, hair, birthdate);
